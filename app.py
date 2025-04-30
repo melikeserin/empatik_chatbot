5f148ab2e1e236943ca0d49e311a09bdc33bebfb
melikeserin <139703192+melikeserin@users.noreply.github.com>
2025-04-30 23:17:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def generate_response(polarity):
     if polarity >= 0.6:
         return " Harika! Gerçekten mutlu görünüyorsun. Enerjini çevrendekilerle paylaşabilirsin."
     elif polarity >= 0.2:
-        return " İyi hissetmene sevindim. Sevdiğin bir şeyle gününü daha da güzelleştirebilirsin."
+        return "İyi hissetmene sevindim. Sevdiğin bir şeyle gününü daha da güzelleştirebilirsin."
     elif -0.2 < polarity < 0.2:
         return " Karışık duygular hissediyor gibisin. Belki bir müzik, yürüyüş ya da biriyle konuşmak iyi gelir."
     elif polarity <= -0.2 and polarity > -0.5:
